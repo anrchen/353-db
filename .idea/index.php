2059a3eee93fe8f0f67f942871c5638a344cf5cd
An Ran Chen <anr_chen@live.concordia.ca>
2016-10-17 11:49:30 -0400

    Created and formated index.php

--- .idea/index.php
+++ .idea/index.php
@@ -1,58 +1,79 @@
 <!DOCTYPE html>
-    <html>
+<html xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
         <head>
             <link rel="stylesheet" type="text/css" href="../css/main.css">
         </head>
         <body>
+            <div id="banner"></div>
             <section class="community">
                 <header class="communityHeader">
                     <div class="siteLogo_container">
                         <img class="siteLogo" src="../Images/logo.jpg"></img>
                         <p class="welcomeMsg">
-                            "Welcome to the official "
+                            Welcome to the official
                             <span class="SiteName">
-                                "Car2Go"
-                            </span> " site"
+                                Car2Go
+                            </span>  site
                         </p>
                     </div>
                 </header>
 
                 <div class="category">
                     <header class="categoryHeader">
-                        <h1 class="commnunityHeading"></h1>
+                        <h1 class="commnunityHeading">Rider</h1>
+                        <button class="search" id="searchField" data-trigger="toggle.search.field" type="button">
+                            <span class="searchButton">
+                                <i class="icon"></i>
+                            </span>
+                        </button>
+                        <input name="q" placeholder="Search all posts" type="search" autocomplete="off">
                     </header>
 
-                    <a href="../Images/rider_createPost"></a>
-                    <a href="../Images/rider_editPost"></a>
-                    <a href="../Images/rider_viewPost"></a>
+                    <a href="../Images/rider_createPost">
+                        <img src="../Images/rider_createPost.png">
+                    </a>
+                    <a href="../Images/rider_editPost">
+                        <img src="../Images/rider_editPost.png">
+                    </a>
+                    <a href="../Images/rider_viewPost">
+                        <img src="../Images/rider_viewPost.png">
+                    </a>
                     <a href="../Images/rider_matchPost"></a>
+                    <a href="../Images/rider_rateATrip"></a>
                 </div>
 
-            <div id="wrapper">
-                <div id="banner">
+                <div class="category">
+                    <header class="categoryHeader">
+                        <h1 class="commnunityHeading">Driver</h1>
+                    </header>
+
+                    <a href="../Images/driver_createPost.jpg">
+                        <img src="../Images/driver_createPost.png">
+                    </a>
+                    <a href="../Images/drider_editPost">
+                        <img src="../Images/driver_editPost.png">
+                    </a>
+                    <a href="../Images/drider_viewPost">
+                        <img src="../Images/driver_viewPost.png">
+                    </a>
+                    <a href="../Images/drider_matchPost"></a>
                 </div>
 
-                <nav id="navigation">
-                    <ul id="nav">
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="#">Ride</a></li>
-                        <li><a href="#">Drive</a></li>
-                        <li><a href="#">About us</a></li>
-                    </ul>
-                </nav>
+                <div class="category">
+                    <header class="categoryHeader">
+                        <h1 class="commnunityHeading">Administrator</h1>
+                    </header>
 
-<!--                <div id="content_area">-->
-<!--                    <--php echo $content;-->
-<!--                </div>-->
-
-
-                <div id="sidebar">
-
+                    <a href="../Images/admin_createPost"></a>
+                    <a href="../Images/admin_editPost"></a>
+                    <a href="../Images/admin_viewPost"></a>
+                    <a href="../Images/admin_matchPost"></a>
                 </div>
+
 
                 <footer>
                     <p>All rights reserved</p>
                 </footer>
-            </div>
+
         </body>
     </html>
