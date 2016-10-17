8a23f95469c3e212128ac1c3e92234eeb166198e
An Ran Chen <anr_chen@live.concordia.ca>
2016-10-17 00:36:20 -0400

    Added css files

---
+++ Driver_Rider_System/.idea/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="../css/main.css">
+        </head>
+        <body>
+            <section class="community">
+                <header class="communityHeader">
+                    <div class="siteLogo_container">
+                        <img class="siteLogo" src="../Images/logo.jpg"></img>
+                        <p class="welcomeMsg">
+                            "Welcome to the official "
+                            <span class="SiteName">
+                                "Car2Go"
+                            </span> " site"
+                        </p>
+                    </div>
+                </header>
+
+                <div class="category">
+                    <header class="categoryHeader">
+                        <h1 class="commnunityHeading"></h1>
+                    </header>
+
+                    <a href="../Images/rider_createPost"></a>
+                    <a href="../Images/rider_editPost"></a>
+                    <a href="../Images/rider_viewPost"></a>
+                    <a href="../Images/rider_matchPost"></a>
+                </div>
+
+            <div id="wrapper">
+                <div id="banner">
+                </div>
+
+                <nav id="navigation">
+                    <ul id="nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="#">Ride</a></li>
+                        <li><a href="#">Drive</a></li>
+                        <li><a href="#">About us</a></li>
+                    </ul>
+                </nav>
+
+<!--                <div id="content_area">-->
+<!--                    <--php echo $content;-->
+<!--                </div>-->
+
+
+                <div id="sidebar">
+
+                </div>
+
+                <footer>
+                    <p>All rights reserved</p>
+                </footer>
+            </div>
+        </body>
+    </html>
