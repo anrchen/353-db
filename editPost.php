6d28b417d94b30e34725a48947b476d701c3499b
An Ran Chen <anr_chen@live.concordia.ca>
2016-10-30 11:13:00 -0400

    Added editPost.php & Fixed a font issue on addPost.php
    Added editPost.php
    Fixed a font issue on addPost.php

---
+++ editPost.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--       jQuery UI Datepicker -->
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script>
+            $( function() {
+                $( "#datepicker" ).datepicker();
+            } );
+        </script>
+        <!--end of Datepicker -->
+
+    </head>
+
+    <body>
+
+    <header class="header-basic">
+        <link rel="stylesheet" type="text/css" href="css/header.css">
+        <link rel="stylesheet" type="text/css" href="css/addPost.css"/>
+
+
+        <div class="header-limiter">
+
+            <h1><a href="index.php">Su<span>per</span></a></h1>
+
+            <nav>
+                <a href="#">Support</a>
+                <a href="#">Log in</a>
+                <a href="#">About</a>
+            </nav>
+        </div>
+    </header>
+
+    <form method="post" action="/post/generalTrip.php">
+        <div class="new_post">
+            <h1 class="head">Edit Trip</h1>
+            <div class="p">
+
+                <label class="formName">Trip Title</label>
+                <div class="textBoxWrapper">
+                    <input type="text" id="formName" title="" value="" maxlength="100" class="inputBox">
+                </div>
+
+                <input type="text" id="datepicker" placeholder="Departure Date">
+                <input title="" type="time" id="timepicker" value="00:00">
+
+                <div class="textBoxWrapper">
+                    <textarea id="formBody" title="" placeholder="Give a short description of your trip!"></textarea>
+                </div>
+
+                <div class="agreement">
+                    By adding this post, you must agree to the terms of the
+                    <a href="https://google.com"
+                       target="_new">license agreement</a>.
+                </div>
+
+                <input type="submit" id="addPost" value="Post Trip">
+            </div>
+        </div>
+    </form>
+    </body>
+</html>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "person";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=person", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//        $sql = "INSERT INTO SAILORS (sname, rating, age)
+//                VALUES ('John', '8', '18')";
+//        $conn->exec($sql);
+//        echo "New record created successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
+
+$conn=null;
+?>
---
+++ endPost.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Archen94
+ * Date: 10/30/2016
+ * Time: 11:10 AM
+ */
