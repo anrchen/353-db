385b8fe497f3e08c28fc8065c03a5ccb91a6d0c3
Sam-Ming <sam_8520@hotmail.com>
2016-11-11 19:56:39 -0500

    cityname

--- queries.sql
+++ queries.sql
@@ -11,13 +11,15 @@ CREATE TABLE trip (
  TID int(11) NOT NULL,
  dDate int(8) NOT NULL,
  aDate int(8) NOT NULL,
+ cityName varchar (10) NOT NULL,
  dPostal varchar(20) DEFAULT NULL,
  aPostal varchar(20) DEFAULT NULL,
  Description varchar(100) DEFAULT NULL,
  Restriction boolean DEFAULT NULL,
  Title varchar(10) DEFAULT NULL,
   Comments varchar(100) DEFAULT NULL,
- PRIMARY KEY (TID)
+ PRIMARY KEY (TID),
+ FOREIGN KEY (cityName) REFERENCES FROM city (cityName)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE account (
