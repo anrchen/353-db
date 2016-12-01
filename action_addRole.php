d756ffb13457ff900f65f6e2f30db941f6d4b708
An Ran Chen <ky_chen93@hotmail.com>
2016-12-01 17:03:40 -0500

    Updated selectRole
    A driver can now become a rider, and rider as a driver

--- action_addRole.php
+++ action_addRole.php
@@ -1,4 +1,18 @@
 <?php
+    session_start();
+    $addRole=$_GET['role'];
+    $MID=$_SESSION['user'];
+    include_once ("connection.php");
 
+    $con=new Connection();
+    if($addRole=='driver'){
+        $sql="UPDATE member SET isDriver=1 WHERE MID=$MID";
+    }else if($addRole=='rider'){
+        $sql="UPDATE member SET isRider=1 WHERE MID=$MID";
+    }
+    $con->setQuery($sql);
+    $con->execute();
+    $con->close();
 
+    header("Location: selectRole.php");
 ?>
